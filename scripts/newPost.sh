3f73c1ece87cecec3881260d1f04e37417d07051
Mark Sharpley <698889+markopolo123@users.noreply.github.com>
2025-03-15 20:04:53 +0000

    ci(newpost.sh): helper script
    Signed-off-by: Mark Sharpley <698889+markopolo123@users.noreply.github.com>

--- scripts/newPost.sh
+++ scripts/newPost.sh
@@ -5,11 +5,18 @@ if [ $# -eq 0 ]
     echo "I can't proceed, I don't have an argument for the post location"
 fi
 
-post_name=$(gum input --placeholder "What name for your post?")
-post_dir="$1"/"$post_name"
+post_name=$(gum input --placeholder "What would you like your post title to be?")
+
+lower_post_name=$(echo "$post_name" | tr "[:upper:]" "[:lower:]")
+
+stripped_post_name=$(echo "${lower_post_name}"| sed -e "s/ /-/g")
+post_dir="$1"/"$stripped_post_name"
 
 mkdir -p "$post_dir"
 
 hugo new content "$post_dir"/index.md
+
+gsed -i "/yourtitle/c\title: $post_name" "$post_dir"/index.md
+
 
 hx "$post_dir"/index.md
